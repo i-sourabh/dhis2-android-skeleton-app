4b1a6b8b14db00b863ad4c9ba2c5ee9485c1c5cc
Sourabh Bhardwaj <bsourabh191@gmail.com>
2019-07-02 15:45:46 +0530

    solution ex01-d2

--- app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
@@ -3,12 +3,23 @@ package com.example.android.androidskeletonapp.data;
 import android.content.Context;
 
 import org.hisp.dhis.android.core.d2manager.D2Configuration;
+import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.d2manager.D2Manager;
+
+import io.reactivex.Single;
 
 public class Sdk {
 
     public static D2Configuration getD2Configuration(Context context) {
         // TODO Add configuration
 
-        return null;
+        D2Configuration configuration=D2Configuration.builder()
+                .appName("test-sou").appVersion("1.0")
+                .context(context)
+                .readTimeoutInSeconds(30).writeTimeoutInSeconds(30).connectTimeoutInSeconds(30)
+                .build();
+        return configuration;
     }
+
+
 }
--- app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.androidskeletonapp.ui.splash;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 
 import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.d2manager.D2Manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Single;
@@ -18,14 +22,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         disposable =
                 instantiateD2()
                 .doOnSuccess(d2 -> {
                     // TODO Toast success
+                    Toast.makeText(this, "instantiateD2-Success", Toast.LENGTH_SHORT).show();
+                    Log.d("instantiateD2-Success","instantiateD2-Success");
+
                     }
                 ).doOnError(throwable -> {
                     // TODO Toast error
+                    Toast.makeText(this, "instantiateD2-Error", Toast.LENGTH_SHORT).show();
+                    Log.d("instantiateD2-Success","instantiateD2-Success");
                 })
                 .subscribe();
     }
@@ -40,6 +48,10 @@ public class SplashActivity extends AppCompatActivity {
 
     private Single<D2> instantiateD2() {
         // TODO Instantiate d2
-        return Single.never();
+        Single<D2> d2Single= D2Manager.setUp(Sdk.getD2Configuration(this)).andThen(D2Manager.setServerUrl("http://android2.dhis2.org:8080"))
+                .andThen(D2Manager.instantiateD2());
+        
+//        return Single.never();
+          return d2Single;
     }
 }
